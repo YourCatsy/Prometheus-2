c80a6ed34b02b200da83980e6bee2647349129da
Catherine <catherineamf@gmail.com>
2024-12-29 19:23:41 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
-
 # Dockerfile
-FROM python:3.9-slim
+FROM node:18-slim
 
 # Установка зависимостей
 WORKDIR /app
 
-# Копирование кода приложения
+# Копирование package.json и package-lock.json (если они есть) для установки зависимостей
+COPY package*.json ./
+
+# Установка зависимостей
+RUN npm install
+
+# Копирование оставшихся файлов
 COPY . .
 
-# Команда для запуска приложения
-CMD ["python", "app.py"]
+# Открытие порта
+EXPOSE 8080
+
+# Запуск приложения
+CMD ["npm", "start"]
--- src/app.js
+++ src/app.js
@@ -3,18 +3,20 @@ const serve = require('koa-static');
 const views = require('koa-views');
 const Koa = require('koa');
 const redis = require('./db/redis');
+const promClient = require('prom-client');  // Добавить импорт для prom-client
 const appPort = require('../config/app');
 
 const app = module.exports = new Koa();
 
-// setup views, appending .ejs
-// when no extname is given to render()
+// Создаем реестр для метрик
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
 
+// Настройка представлений
 app.use(serve(`${__dirname}/public`));
 app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }));
 
-// dummy data
-
+// Данные пользователя для рендера
 const user = {
   name: {
     first: 'Tobi',
@@ -24,10 +26,18 @@ const user = {
   age: 3
 };
 
-// render
+// Маршрут для получения метрик Prometheus
+app.use(async (ctx, next) => {
+  if (ctx.url === '/metrics') {
+    ctx.set('Content-Type', register.contentType);
+    ctx.body = await register.metrics();
+  } else {
+    await next();
+  }
+});
 
+// Основной маршрут рендера данных пользователя
 app.use(async function(ctx) {
-  
   if (ctx.url === '/redis') {
     const data = await redis.ping();
     return ctx.render('redis', { redisData: data });
@@ -35,4 +45,5 @@ app.use(async function(ctx) {
   return ctx.render('user', { user });
 });
 
+// Запуск приложения
 if (!module.parent) app.listen(appPort);
